0d54d412cdfdd8e60cf0eebaf58b351e094953c6
1smat <reven13022002@gmail.com>
2022-11-13 18:43:36 +0500

    form

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <script setup>
-import routes from "../routes/routes.js"
-import ScrollReveal from "scrollreveal"
-import { ref } from "@vue/reactivity"
-import { onMounted } from "@vue/runtime-core"
+import routes from '../routes/routes.js'
+import ScrollReveal from 'scrollreveal'
+import { ref } from '@vue/reactivity'
+import { onMounted } from '@vue/runtime-core'
 
 const navToggle = ref(null)
 const navMenu = ref(null)
@@ -13,26 +13,26 @@ onMounted(() => {
         const nav = navMenu.value
 
         if (toggle && nav) {
-            toggle.addEventListener("click", () => {
-                nav.classList.toggle("show")
-                document.body.classList.toggle("stop-scrolling")
+            toggle.addEventListener('click', () => {
+                nav.classList.toggle('show')
+                document.body.classList.toggle('stop-scrolling')
             })
         }
     }
     showMenu()
 
-    const navLink = document.querySelectorAll(".nav__link")
+    const navLink = document.querySelectorAll('.nav__link')
 
     function linkAction() {
-        const navMenu = document.getElementById("nav-menu")
+        const navMenu = document.getElementById('nav-menu')
         // When we click on each nav__link, we remove the show-menu class
-        navMenu.classList.remove("show")
-        document.body.classList.toggle("stop-scrolling")
+        navMenu.classList.remove('show')
+        document.body.classList.toggle('stop-scrolling')
     }
-    navLink.forEach((n) => n.addEventListener("click", linkAction))
+    navLink.forEach((n) => n.addEventListener('click', linkAction))
 
     /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-    const sections = document.querySelectorAll("section[id]")
+    const sections = document.querySelectorAll('section[id]')
 
     function scrollActive() {
         const scrollY = window.pageYOffset
@@ -40,38 +40,38 @@ onMounted(() => {
         sections.forEach((current) => {
             const sectionHeight = current.offsetHeight
             const sectionTop = current.offsetTop - 50
-            const sectionId = current.getAttribute("id")
+            const sectionId = current.getAttribute('id')
 
             // console.log(current, "cureent")
 
             if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
                 document
                     .querySelector(`.nav__menu a[href*="${sectionId}"]`)
-                    .classList.add("active")
+                    .classList.add('active')
             } else {
                 document
                     .querySelector(`.nav__menu a[href*="${sectionId}"]`)
-                    .classList.remove("active")
+                    .classList.remove('active')
             }
         })
     }
-    window.addEventListener("scroll", scrollActive)
+    window.addEventListener('scroll', scrollActive)
 
     /*===== SCROLL REVEAL ANIMATION =====*/
     const sr = ScrollReveal({
-        origin: "top",
-        distance: "60px",
+        origin: 'top',
+        distance: '60px',
         duration: 2000,
         delay: 200,
         //     reset: true
     })
 
-    sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text", {})
-    sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
+    sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {})
+    sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', {
         delay: 400,
     })
-    sr.reveal(".home__social-icon", { interval: 200 })
-    sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 })
+    sr.reveal('.home__social-icon', { interval: 200 })
+    sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 })
 })
 </script>
 <template>
